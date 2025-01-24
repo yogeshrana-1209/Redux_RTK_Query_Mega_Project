85801ea153a0decdede00d7c11ae5458b103d104
Yogesh <yr70416@gmail.com>
2025-01-24 17:22:13 +0530

    AlbumsList is Updated

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,39 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";  
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
+  
   const { data, error, isLoading } = useFetchAlbumsQuery(user.id);
 
-  console.log(data, error, isLoading);
-  console.log(user);
+  // const { data, error, isLoading } = useFetchAlbumsQuery(user.id);
+  // console.log(data, error, isLoading);
+  // console.log(user);
 
-  return <div> Albums for {user.name}</div>;
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  }else if (error) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map(album => {
+      const header = <div> {album.title} </div>
+        return <ExpandablePanel key={album.id} header={header}>
+            List of Photos in the album
+          </ExpandablePanel>     
+    });
+
+  return <div> 
+      <div>
+      Albums for {user.name}
+        </div>
+        <div>
+          {content}
+        </div>
+    </div>
+  }
 }
 
 export default AlbumsList;
