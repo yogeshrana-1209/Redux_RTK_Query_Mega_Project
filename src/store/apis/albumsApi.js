4350ff542ad8a736c2578f5cbeafbc3516e9f488
Yogesh Rana <yr70416@gmail.com>
2025-01-24 16:19:59 +0530

    add comment in api file
    just add comment

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// Create the Custom API
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
